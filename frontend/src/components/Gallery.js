bdc67aedeecaec12b8fe437bff4c9f21da0c40ee
aqdamzh <aqdamzain1@gmail.com>
2023-08-13 02:20:03 +0700

    add fetch videos: error

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -1,5 +1,5 @@
 import style from "./Gallery.module.css";
-
+import { useState, useEffect } from "react";
 import {
      Wrap,
      WrapItem,
@@ -9,20 +9,53 @@ import {
     } from "@chakra-ui/react";
 
 import ImgPng from './videodefault.jpg';
+import { useNavigate } from "react-router-dom";
+
+const getAllVideos = async () => {
+    const url = 'http://localhost:8080/api/videos';
+    const responseData = await fetch(url, {
+        method: 'GET'
+    })
+    .then( response => {
+        if(!response.ok){
+            throw new Error('HTTP status ' + response.status);
+        }else {
+            return response.json();
+        }
+    });
+    return responseData;
+}
 
 function Gallery() {
+
+    const [videos, setVideos] = useState([]);
+    const navigate = useNavigate();
     const mainData = [];
+
     for(let i=0; i<100; i++){
       mainData.push(`data ${i}`);
     }
+    const handleDetail = () => {
+        navigate('/video');
+    }
+    useEffect(() => {
+        getAllVideos()
+        .then(data => {
+            setVideos(data);
+        });
+    }, []);
     return (
     <Stack direction='column'>
+    {console.log(videos)}
     <Heading>Codingpedia Play</Heading>
     <Wrap p={12} spacing='30px' justify='center' bg='gray.100'>
         {mainData.map(
             (data) => (
             <WrapItem>
-                <Image objectFit='cover' src={ImgPng} className={style['videoTumbX']} />
+                <Image objectFit='cover' 
+                src={ImgPng} 
+                className={style['videoTumbX']} 
+                onClick={handleDetail} />
             </WrapItem>
             )
         )}
